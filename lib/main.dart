96ced62d830489b08e5c64c2af3c4f47aa344ab3
ukkhhan <ukkhan647@gmail.com>
2026-06-11 17:06:05 +0500

    configured firebase to the project.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app.dart';
-// import 'firebase_options.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Initialize Firebase
-  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const ProviderScope(child: LedgifyApp()));
 }
